4e7f62ec80bc57ec51f19d9f7dfb4d839e70f1b6
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-07-05 08:52:59 -0400

    Comments

--- 12/demos/csproj-msbuild_core2/end/Library/src/Library.API/Helpers/AuthorsResourceParameters.cs
+++ 12/demos/csproj-msbuild_core2/end/Library/src/Library.API/Helpers/AuthorsResourceParameters.cs
@@ -5,29 +5,36 @@ using System.Threading.Tasks;
 
 namespace Library.API.Helpers
 {
+// ****** This will be specified as a Controller's Action Parmater so that if the Http Request contains any key / value pairs whose key matches 
+//        the public properties of this class then the value will be populated and the Controller's Action will have access toit.
     public class AuthorsResourceParameters
     {
-        const int maxPageSize = 20;
-        public int PageNumber { get; set; } = 1;
+        // For PAGING 
+            const int maxPageSize = 20;
+            public int PageNumber { get; set; } = 1;
 
-        private int _pageSize = 10;
-        public int PageSize
-        {
-            get
+            private int _pageSize = 10;
+            public int PageSize
             {
-                return _pageSize;
+                get
+                {
+                    return _pageSize;
+                }
+                set
+                {
+                    _pageSize = (value > maxPageSize) ? maxPageSize : value;
+                }
             }
-            set
-            {
-                _pageSize = (value > maxPageSize) ? maxPageSize : value;
-            }
-        }
 
-        public string Genre { get; set; }
+        // For FILTERING
+            public string Genre { get; set; }
 
-        public string SearchQuery { get; set; }
+        // For SEACRHING
+            public string SearchQuery { get; set; }
 
-        public string OrderBy { get; set; } = "Name";
+        // For SORTING
+        //  ere we are giving it a default value
+            public string OrderBy { get; set; } = "Name";
 
         public string Fields { get; set; }
     }
