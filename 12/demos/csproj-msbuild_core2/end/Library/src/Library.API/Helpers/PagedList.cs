2d424b5c084279113c2730eda62da054626cc150
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-07-05 13:02:49 -0400

    Comments

--- 12/demos/csproj-msbuild_core2/end/Library/src/Library.API/Helpers/PagedList.cs
+++ 12/demos/csproj-msbuild_core2/end/Library/src/Library.API/Helpers/PagedList.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 
 namespace Library.API.Helpers
 {
+
+    // Subclassing because we want a List<T> but want some additional functionality for Paging
     public class PagedList<T> : List<T>
     {
         public int CurrentPage { get; private set; }
@@ -28,6 +30,7 @@ namespace Library.API.Helpers
             }
         }
 
+        // CTOR
         public PagedList(List<T> items, int count, int pageNumber, int pageSize)
         {
             TotalCount = count;
@@ -37,10 +40,15 @@ namespace Library.API.Helpers
             AddRange(items);
         }
 
+       // Whoever calls this method will provide the T
+       // and the first parameter should be an IQueryable of that T
         public static PagedList<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = source.Count();
-            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            var items = source.Skip((pageNumber - 1) * pageSize)
+                              .Take(pageSize)
+                              .ToList();                                // EXECUTE THE QUERY
+                              
             return new PagedList<T>(items, count, pageNumber, pageSize);
         }
     }
