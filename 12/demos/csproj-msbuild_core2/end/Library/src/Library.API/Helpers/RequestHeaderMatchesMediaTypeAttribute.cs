106ace166fde7b1316116d50c75845db44ba5db6
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-07-07 19:11:09 -0400

    Comments

--- 12/demos/csproj-msbuild_core2/end/Library/src/Library.API/Helpers/RequestHeaderMatchesMediaTypeAttribute.cs
+++ 12/demos/csproj-msbuild_core2/end/Library/src/Library.API/Helpers/RequestHeaderMatchesMediaTypeAttribute.cs
@@ -12,42 +12,28 @@ namespace Library.API.Helpers
         private readonly string[] _mediaTypes;
         private readonly string _requestHeaderToMatch;
 
-        public RequestHeaderMatchesMediaTypeAttribute(string requestHeaderToMatch,
-            string[] mediaTypes)
+        public RequestHeaderMatchesMediaTypeAttribute(string requestHeaderToMatch, string[] mediaTypes)
         {
             _requestHeaderToMatch = requestHeaderToMatch;
             _mediaTypes = mediaTypes;
         }
 
-        public int Order
-        {
-            get
-            {
-                return 0;
-            }
-        }
+        public int Order  { get { return 0;  }  }
 
         public bool Accept(ActionConstraintContext context)
         {
-            var requestHeaders = context.RouteContext.HttpContext.Request.Headers;
-
-            if (!requestHeaders.ContainsKey(_requestHeaderToMatch))
-            {
+            var reqHeaders = context.RouteContext.HttpContext.Request.Headers;
+            if (!reqHeaders.ContainsKey(_requestHeaderToMatch))  {
                 return false;
             }
 
             // if one of the media types matches, return true
             foreach (var mediaType in _mediaTypes)
             {
-                var mediaTypeMatches = string.Equals(requestHeaders[_requestHeaderToMatch].ToString(),
-                    mediaType, StringComparison.OrdinalIgnoreCase);
+                var match = string.Equals(reqHeaders[_requestHeaderToMatch].ToString(), mediaType, StringComparison.OrdinalIgnoreCase);
 
-                if (mediaTypeMatches)
-                {
-                    return true;
-                }
+                if (match) { return true; }
             }
-
             return false;
         }
     }
